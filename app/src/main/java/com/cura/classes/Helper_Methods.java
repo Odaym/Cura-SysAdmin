41163ec44f3db690e4e00d4858117efa13a027b5
Oday Maleh <oday.maleh@gmail.com>
2014-10-30 19:08:04 +0200

    AppendLog method added

--- app/src/main/java/com/cura/classes/Helper_Methods.java
+++ app/src/main/java/com/cura/classes/Helper_Methods.java
@@ -1,57 +1,96 @@
 package com.cura.classes;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URISyntaxException;
-
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Helper_Methods {
-	public static String getPath(Context context, Uri uri)
-			throws URISyntaxException {
-		if ("content".equalsIgnoreCase(uri.getScheme())) {
-			String[] projection = { "_data" };
-			Cursor cursor = null;
 
-			try {
-				cursor = context.getContentResolver().query(uri, projection, null,
-						null, null);
-				int column_index = cursor.getColumnIndexOrThrow("_data");
-				if (cursor.moveToFirst()) {
-					return cursor.getString(column_index);
-				}
-			} catch (Exception e) {
-				// Eat it
-			}
-		} else if ("file".equalsIgnoreCase(uri.getScheme())) {
-			return uri.getPath();
-		}
+    public static void appendLog(String text) {
+        Log.d("LOGFILE", text);
+        SimpleDateFormat sTime = new SimpleDateFormat("dd/MMM/yyyy - hh:mm:ss");
+        String timeFormat = sTime.format(new Date());
 
-		return null;
-	}
+        File sdCard = Environment.getExternalStorageDirectory();
+        File dir = new File(sdCard.getAbsolutePath() + "/Cura/Logs/");
+        dir.mkdirs();
+        File logFile = new File(dir, "Cura_Logs_DEBUG.txt");
+        if (!logFile.exists()) {
+            try {
+                logFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            BufferedWriter buf = new BufferedWriter(new FileWriter(logFile, true));
+            if (text.compareTo("wipe") == 0)
+                logFile.delete();
+            else {
+                buf.append("[" + timeFormat + "] - ");
+                buf.append(text);
+                buf.newLine();
+                buf.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static String convertStreamToString(InputStream is) throws Exception {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-		StringBuilder sb = new StringBuilder();
-		String line = null;
-		while ((line = reader.readLine()) != null) {
-			sb.append(line).append("\n");
-		}
-		reader.close();
-		return sb.toString();
-	}
+    public static String getPath(Context context, Uri uri)
+            throws URISyntaxException {
+        if ("content".equalsIgnoreCase(uri.getScheme())) {
+            String[] projection = {"_data"};
+            Cursor cursor = null;
 
-	public static String getStringFromFile(String filePath) throws Exception {
-		File fl = new File(filePath);
-		FileInputStream fin = new FileInputStream(fl);
-		String ret = convertStreamToString(fin);
-		// Make sure you close all streams.
-		fin.close();
-		return ret;
-	}
+            try {
+                cursor = context.getContentResolver().query(uri, projection, null,
+                        null, null);
+                int column_index = cursor.getColumnIndexOrThrow("_data");
+                if (cursor.moveToFirst()) {
+                    return cursor.getString(column_index);
+                }
+            } catch (Exception e) {
+                // Eat it
+            }
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    public static String convertStreamToString(InputStream is) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        reader.close();
+        return sb.toString();
+    }
+
+    public static String getStringFromFile(String filePath) throws Exception {
+        File fl = new File(filePath);
+        FileInputStream fin = new FileInputStream(fl);
+        String ret = convertStreamToString(fin);
+        // Make sure you close all streams.
+        fin.close();
+        return ret;
+    }
 }

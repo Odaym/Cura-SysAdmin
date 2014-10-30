daefd8432a8816f9a241f809b633cbfbcf6139cb
Oday Maleh <oday.maleh@gmail.com>
2014-10-30 19:09:50 +0200

    App domain name added for rating feature

--- app/src/main/java/com/cura/classes/Constants.java
+++ app/src/main/java/com/cura/classes/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
 	public static final String CuraFont = "Courier";
 	public static final String HOCKEY_APP_ID = "12481c04a3649625dc7f00563840932f";
 	public static final String FLURRY_APP_ID = "GMPCS9FXNNWTTG2THY8G";
+    public static final String APP_MARKET_NAME = "com.cura";
 }
